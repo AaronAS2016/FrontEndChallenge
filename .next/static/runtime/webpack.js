b1227e5a2398f1cf68427f0111b0a17bb668909c
Aaron <aaronsaban98@gmail.com>
2019-01-12 00:33:25 -0300

    added menu

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "415890608ca0ba12b320";
+/******/ 	var hotCurrentHash = "dcd02d5bce1d802a88f6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
